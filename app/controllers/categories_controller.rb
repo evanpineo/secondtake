0632e7ee4e025accfff81d19674bb1917a662a25
evanpineo <evan.pineo@gmail.com>
2017-05-15 23:57:08 +0000

    add equipment feature

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,40 @@ class CategoriesController < ApplicationController
   
   def index
     @categories = Category.all
+    # @categories = Category.order(params[:sort]).all
   end
-
+  
+  def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.create(category_params)
+    if @category.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end  
+  end
+  
+  def show
+    @category = Category.find(params[:id])
+  end
+  
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    @category = Category.find(params[:id])
+    @category.update_attributes(category_params)
+    redirect_to root_path
+  end
+  
+  private
+  
+  def category_params
+    params.require(:category).permit(:manufacturer, :model, :specs)
+  end
+  
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  validates :manufacturer, length: { minimum: 2 }, presence: true
+  validates :model, length: { minimum: 2 }, presence: true
+  validates :specs, presence: true
 end
